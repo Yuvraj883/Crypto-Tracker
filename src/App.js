14c05fa548efb1a16793cad69e1de9379837e782
Yuvraj <kingyuvi88@gmail.com>
2023-06-11 00:18:45 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const url = 'https://fakestoreapi.com/products'; 
-  // const key = '42ac69f7fcmshf12ad123d54ec4bp165600jsnf5552041810b'; 
-  
-  async function logJSONData() {
-      const response = await fetch(url,{
-        method:'GET',
-        // headers:{
-        //   'X-RapidAPI-Key': key,
-	      // 	'X-RapidAPI-Host': 'coingecko.p.rapidapi.com'
-        // }
-      });
-      const jsonData = await response.json();
-      console.log(jsonData);
-    }
-  
-    logJSONData();
-  
+   
   return (
     <div className="App">
       
