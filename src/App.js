4a80b358c7bab7697adf2331f8273eec65cf9539
Yuvraj <kingyuvi88@gmail.com>
2023-06-11 21:54:56 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-
+console.log("Hi from APP"); 
   return (
-    <div className="App">
-      
+    <div className="bg-black">
+      <Navbar></Navbar>
+      hi
     </div>
   );
 }
