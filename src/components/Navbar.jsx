416c2e350fcdbd5b8933c9b4c99357af47756f6f
Yuvraj <kingyuvi88@gmail.com>
2023-06-11 21:56:14 +0530

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const Navbar = ()=>{
     return(
         <>
-        <nav>
-            
+        <nav className="bg-black h-12 text-white">
+            <h1>Crypto Tracker</h1>
         </nav>
         </>
     )
 }
+export default Navbar; 
