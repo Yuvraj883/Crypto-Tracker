f5138fcbadea0a2d3f2aa06506006fd0442f58d4
Yuvraj <kingyuvi88@gmail.com>
2023-06-29 02:21:16 +0530

    Created search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar = () => {
+    return (
+        <>
+        <div className="flex flex-row justify-center items-center  m-10">
+            <input className=" py-2 bg-gray-100 w-[80%] rounded-lg " placeholder="Search coin..."/>
+        </div>
+
+        </>
+    )
+}
+
+export default SearchBar; 
