5791c5be9e48dafe9d197516f25167ab5224a137
Yuvraj <kingyuvi88@gmail.com>
2023-06-29 02:11:46 +0530

    Created Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Navbar = ()=>{
     return(
         <>
-        <nav className="bg-black h-12 text-white">
-            <h1>Crypto Tracker</h1>
+        <nav className="bg-black flex items-center p-4 ">
+            <h1 className="text-3xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-red-600 to-purple-500">Crypto Tracker</h1>
         </nav>
         </>
     )
