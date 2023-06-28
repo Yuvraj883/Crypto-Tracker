59d2ea96097a9eda4d8676f2fa0f24a1776c3e7e
Yuvraj <kingyuvi88@gmail.com>
2023-06-29 02:32:14 +0530

    Created Crypto Table

---
+++ src/components/CryptoTable.jsx
@@ -0,0 +1,19 @@
+const CryptoTable = ()=>{
+    return(
+        <>
+        <div className="w-[80%] mx-auto">
+            <div className="text-[#A0AEC0] font-semibold flex flex-col text-xl justify-between items-ceter p-2 border-1 border-solid border-white">
+              <div className="flex flex-row text-lg justify-between">
+              <span>COIN</span>
+                <span>SYMBOL</span>
+                <span>CURRENT PRICE</span>
+                <span>PRICE GRAPH</span>
+                <span>ORDER VALUE</span>
+                <span>MARKET DEPTH</span>
+              </div>
+            </div>
+            </div>
+        </>
+    )
+}
+export default CryptoTable;
