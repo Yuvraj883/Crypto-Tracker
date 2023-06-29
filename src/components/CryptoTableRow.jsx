3724a9cd4c8cc20ba90d32d2d16f8b4be41452c9
Yuvraj <kingyuvi88@gmail.com>
2023-06-29 14:47:58 +0530

    Added desgin for small screen devices

--- src/components/CryptoTableRow.jsx
+++ src/components/CryptoTableRow.jsx
@@ -20,13 +20,13 @@ const CryptoTableRow = ({index, symbol, name, image, current_price, price_change
         {/* Mobile View  */}
         <div className={index%2===0?'bg-[#2d3748] md:hidden flex flex-row justify-between text-sm items-center p-2': 'bg-gray-900 md:hidden flex flex-row justify-between text-sm items-center p-2'}>
             <span className="w-[25%] "><img src={image} className="h-8 w-auto inline" alt="coin-img"/> {name}</span>
-            <span className="w-[15%] text-center">{symbol}</span>
-            <span className="w-[25%] text-center">{current_price.toLocaleString('en-IN', {
+            <span className="w-[15%] flex flex-row justify-start">{symbol}</span>
+            <span className="w-[25%] flex flex-row justify-start">{current_price.toLocaleString('en-IN', {
                 currency:'INR',
                 style:'currency',
                 maximumFractionDigits:2
             })}</span>
-            <span  className={price_change_percentage_24h.toFixed(2)<0?"w-[20%] text-red-700 text-center p-2":"w-[20%] text-center text-green-700 "}>{price_change_percentage_24h.toFixed(2)}%</span>
+            <span  className={price_change_percentage_24h.toFixed(2)<0?"w-[20%] text-red-700 text-center p-2 flex flex-row justify-start":"w-[20%] text-center text-green-700 flex flex-row justify-start "}>{price_change_percentage_24h.toFixed(2)}%</span>
             {/* <span className="w-[15%]">{total_volume.toLocaleString('en-IN')} </span>
 
             <span  className="w-[15%]">{market_cap.toLocaleString('en-IN')}</span> */}
