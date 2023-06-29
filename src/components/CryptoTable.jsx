e1edd6259cef30f0288e6b9f766f5e8b8625d2ef
Yuvraj <kingyuvi88@gmail.com>
2023-06-29 14:43:53 +0530

    Added design for small screen devices

--- src/components/CryptoTable.jsx
+++ src/components/CryptoTable.jsx
@@ -51,8 +51,8 @@ const [data, setData] = useState([]);
     }
     return(
         <>
-        <div className="w-[90%] mx-auto ">
-            <div className="text-[#A0AEC0] font-semibold flex flex-col text-xl justify-between items-ceter  border-[1px] border-solid border-white">
+        <div className="w-[90%] mx-auto md:block hidden">
+            <div className="text-[#A0AEC0] font-semibold flex flex-col text-xl justify-between items-ceter  border-[.5px] border-solid border-white">
                 <div className="flex flex-row text-lg justify-between p-2">
               <span className="w-[20%] p-2">COIN</span>
                 <span  className="w-[10%] ">SYMBOL</span>
@@ -71,6 +71,29 @@ const [data, setData] = useState([]);
 
             </div>
             </div>
+
+            {/* Mobile View */}
+            <div className="w-[98%] mx-auto md:hidden block ">
+            <div className="text-[#A0AEC0] font-semibold flex flex-col text-md justify-between items-ceter  border-[.5px] border-solid border-white">
+                <div className="flex flex-row text-base justify-between p-2">
+              <span className="w-[25%] mx-1">COIN</span>
+                <span  className="w-[15%] mx-1 ">SYMBOL</span>
+                <span  className="w-[25%] mx-1 ">CURRENT PRICE</span>
+                <span  className="w-[20%] mx-1">PRICE GRAPH</span>
+                {/* <span  className="w-[15%]">ORDER VALUE</span>
+                <span  className="w-[15%]">MARKET DEPTH</span> */}
+              </div>
+            {
+                   data.map((obj, index)=>(
+                    <CryptoTableRow {...obj} index={index} key={index}/> 
+                ))
+            }
+               
+              
+
+            </div>
+            </div>
+
         </>
     )
 }
